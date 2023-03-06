719d53f39fe64f1c29f8634a1569b65a930f82d8
saorixvy <125274992+saorixvy@users.noreply.github.com>
2023-03-06 14:52:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+let dado = 0
+input.onGesture(Gesture.Shake, function () {
+    dado = randint(1, 6)
+    if (1 == dado) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (2 == dado) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (3 == dado) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (4 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (5 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (6 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
